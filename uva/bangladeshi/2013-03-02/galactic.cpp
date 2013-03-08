0f40c09b66d786708121246ad309ac7618f42977
jccarvalhosa <jocarvalhosa@gmail.com>
2013-03-08 09:57:43 -0300

    simplificando

--- uva/bangladeshi/2013-03-02/galactic.cpp
+++ uva/bangladeshi/2013-03-02/galactic.cpp
@@ -7,10 +7,10 @@
 #include <cmath>
 using namespace std;
 #define MAXD 10000001
-#define MAXN 500001
+#define MAXN 2000000
 
-int id[MAXD], d[2][MAXN], N;
-vector<int> v[MAXN];
+int id[MAXD], d[2][MAXN];
+vector<int> v[MAXN+1];
 
 int query(int x, int y) {
 	int ans=0;
@@ -19,7 +19,7 @@ int query(int x, int y) {
 }
 
 void add(int x, int y) {
-	for( ; x <= N; x += x&-x) v[x].push_back(y);
+	for( ; x <= MAXN; x += x&-x) v[x].push_back(y);
 }
 
 int main() {
@@ -28,26 +28,23 @@ int main() {
 	scanf("%d", &n);
 	for(i=0;i<2;i++) scanf("%lf%lf%lf", &xc[i], &yc[i], &zc[i]);
 	n-=2;
-	memset(id, 0, sizeof(id));
 	for(i=0;i<n;i++) {
 		scanf("%lf%lf%lf", &xp, &yp, &zp);
 		for(j=0;j<2;j++) d[j][i] = ceil(sqrt((xp-xc[j])*(xp-xc[j]) + (yp-yc[j])*(yp-yc[j]) + (zp-zc[j])*(zp-zc[j])));
-		id[d[0][i]]++;
 	}
-	id[0]=1;
-	for(i=1;i<MAXD;i++) id[i] += id[i-1];
-	N = id[MAXD-1];
-	for(i=0;i<n;i++) add(id[d[0][i]], d[1][i]);
-	for(i=1;i<=N;i++) sort(v[i].begin(), v[i].end());
+	for(i=0;i<n;i++) add(d[0][i], d[1][i]);
+	for(i=1;i<=MAXN;i++) sort(v[i].begin(), v[i].end());
 	sort(d[0], d[0]+n);
 	sort(d[1], d[1]+n);
 	stringstream ss;
 	scanf("%d", &q);
 	while(q--) {
 		scanf("%d %d", &i, &j);
+		i = min(i, MAXN);
+		j = min(j, MAXN);
 		a = upper_bound(d[0], d[0]+n, i) - d[0];
 		b = upper_bound(d[1], d[1]+n, j) - d[1];
-		c = query(id[i], j);
+		c = query(i, j);
 		ss << a+b-c << '\n';
 	}
 	cout << ss.str();
