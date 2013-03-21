e6cebc09123fdf524b895c1dba22644f8a781450
jccarvalhosa <jocarvalhosa@gmail.com>
2013-03-20 22:22:03 -0300

    round 174

--- codeforces/round174/cow_tennis_tournament.cpp
+++ codeforces/round174/cow_tennis_tournament.cpp
@@ -5,111 +5,87 @@ using namespace std;
 typedef pair<int, int> pii;	
 typedef long long ll;
 
-struct interval {
-	int zero, one, pending;
-	interval(int zero=1, int one=0, int pending=0): zero(zero), one(one), pending(pending) {}
+struct inter {
+	int zero, one, w;
+	inter(int zero=1, int one=0, int w=0): zero(zero), one(one), w(w) {}
 };
 
-struct interval M[500000];
+struct inter M[500000];
 
-void get_children_values(int node) {
-	M[node].one = M[2*node].one + M[2*node+1].one;
-	M[node].zero = M[2*node].zero + M[2*node+1].zero;
+void sync(int n) {
+	M[n].one = M[2*n].one + M[2*n+1].one;
+	M[n].zero = M[2*n].zero + M[2*n+1].zero;
 }
 
-void shift_values(int node) {
-	if(M[node].pending) swap(M[node].one, M[node].zero);
+void propagate(int n, int b, int e) {
+	M[2*n].w += M[n].w;
+	M[2*n+1].w += M[n].w;
 }
 
-void propagate_pending(int node, int b, int e) {
-	M[2*node].pending += M[node].pending;
-	M[2*node+1].pending += M[node].pending;
+void fix(int n, int b, int e) {
+	M[n].w %= 2;
+	if(M[n].w) swap(M[n].one, M[n].zero);
+	if(b != e) propagate(n, b, e);
+	M[n].w = 0;
 }
 
-void fix_pending(int node, int b, int e) {
-	M[node].pending %= 2;
-	shift_values(node);
-	if(b != e) propagate_pending(node, b, e);
-	M[node].pending = 0;
+inter query(int n, int b, int e, int i, int j) {
+	fix(n, b, e);
+	if(j<b || i>e) return inter(-1);
+	if(i<=b && j>=e) return M[n];
+	inter p1 = query(2*n, b, (b+e)/2, i, j);
+	inter p2 = query(2*n+1, (b+e)/2+1, e, i, j);
+	if(p1.zero == -1) return p2;
+	if(p2.zero == -1) return p1;
+	return inter(p1.zero+p2.zero, p1.one+p2.one);
 }
 
-interval join(interval i1, interval i2) {
-	return interval(i1.zero+i2.zero, i1.one+i2.one);
-}
-
-interval query(int node, int b, int e, int i, int j) {
-	interval p1, p2;
-	fix_pending(node, b, e);
-	if(j<b || i>e) {
-		p1.one = -1;
-		return p1;
-	}
-	if(i<=b && j>=e) return M[node];
-	p1 = query(2*node, b, (b+e)/2, i, j);
-	p2 = query(2*node+1, (b+e)/2+1, e, i, j);
-	if(p1.one == -1) return p2;
-	if(p2.one == -1) return p1;
-	return join(p1, p2);
-}
-
-void update(int node, int b, int e, int i, int j) {
+void update(int n, int b, int e, int i, int j) {
 	if(i<=b && j>=e) {
-		M[node].pending++;
-		fix_pending(node, b, e);
+		M[n].w++;
+		fix(n, b, e);
 	}
-	else if(j<b || i>e) fix_pending(node, b, e);
+	else if(j<b || i>e) fix(n, b, e);
 	else {
-		propagate_pending(node, b, e);
-		M[node].pending=0;
-		update(2*node, b, (b+e)/2, i, j);
-		update(2*node+1, (b+e)/2+1, e, i, j);
-		get_children_values(node);
+		propagate(n, b, e);
+		update(2*n, b, (b+e)/2, i, j);
+		update(2*n+1, (b+e)/2+1, e, i, j);
+		M[n].w=0;
+		sync(n);
 	}
 }
 
-void initialize(int node, int b, int e) {
-	if(b==e) M[node] = interval();
+void init(int n, int b, int e) {
+	if(b==e) M[n] = inter();
 	else {
-		M[node].pending=0;
-		initialize(2*node, b, (b+e)/2);
-		initialize(2*node+1, (b+e)/2+1, e);
-		get_children_values(node);
+		init(2*n, b, (b+e)/2);
+		init(2*n+1, (b+e)/2+1, e);
+		M[n].w=0;
+		sync(n);
 	}
 }
 
-int n, s[100002];
-pii p[100002];
-vector<pii> vs, vf;
+int n, s[100001];
+vector<int> vr[100001], vl[100001];
 
 int main() {
-	int i, j, k, deg;
-	interval left, right;
+	int i, j, k, deg, a, b;
+	inter left, right;
 	cin >> n >> k;
 	for(i=1;i<=n;i++) cin >> s[i];
 	sort(s+1, s+n+1);
-	vs.resize(k+1);
-	vf.resize(k+1);
-	for(i=1;i<=k;i++) {
-		cin >> p[i].first >> p[i].second;
-		p[i].first = (lower_bound(s+1, s+n+1, p[i].first) - s);
-		p[i].second = (upper_bound(s+1, s+n+1, p[i].second) - s) - 1;
-		vs[i] = pii(p[i].first, i);
-		vf[i] = pii(p[i].second, i);
+	for(i=0;i<k;i++) {
+		cin >> a >> b;
+		a = lower_bound(s+1, s+n+1, a) - s;
+		b = upper_bound(s+1, s+n+1, b) - s - 1;
+		vr[a].push_back(b);
+		vl[b].push_back(a);
 	}
-	sort(vs.begin(), vs.end());
-	sort(vf.begin(), vf.end());
 	ll unbalanced = 0;
-	int ns=1, nf=1;
-	initialize(1, 0, n+1);
+	init(1, 0, n+1);
 	for(i=1;i<=n;i++) {
-		while(ns <= k && vs[ns].first <= i) {
-			j = vs[ns++].second;
-			update(1, 0, n+1, p[j].first, p[j].second);
-		}
-		while(nf <= k && vf[nf].first < i) {
-			j = vf[nf++].second;
-			update(1, 0, n+1, p[j].first, p[j].second);
-		}
+		for(j=0;j<vr[i].size();j++) update(1, 0, n+1, i, vr[i][j]);
+		for(j=0;j<vl[i-1].size();j++) update(1, 0, n+1, vl[i-1][j], i-1);
 		left = query(1, 0, n+1, 0, i-1);
 		right = query(1, 0, n+1, i+1, n+1);
 		deg = left.zero + right.one - 1;
