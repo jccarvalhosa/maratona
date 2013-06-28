7764fb55f7f3430cdd8742e5676e8ae516d7972c
jccarvalhosa <jocarvalhosa@gmail.com>
2013-06-27 22:50:44 -0300

    round 183

--- codeforces/round183/modular.cpp
+++ codeforces/round183/modular.cpp
@@ -1,36 +1,27 @@
 #include <cstdio>
-#include <cstring>
-#include <algorithm>
 #include <iostream>
 using namespace std;
+#define M 1000000
 
-int a[5000], vis[1000001], f[1000001];
+int mod(int a) {return a>0?a:-a;}
 
-int mod(int a) { return a > 0 ? a : -a; }
+int a[5000], p[M+1], vis[M+1];
 
 int main() {
-	int n, k, i, j, v, m=0, penal;
+	int n, k, i, j, m, t;
 	cin>>n>>k;
 	for(i=0;i<n;i++) cin>>a[i];
-	memset(f, 0, sizeof(f));
-	for(i=0;i<n;i++) for(j=i+1;j<n;j++) {
-		v = mod(a[i]-a[j]);
-		for(int k=1;k<=5;k++) if(v % k == 0) f[v/k]++;
-	}
-	for(m=1;;m++) {
-		if(f[m] > k*(k+1)/2) continue;
-		penal=0;
-		for(i=0;i<m;i++) vis[i]=0;
+	for(i=0;i<n;i++) for(j=i+1;j<n;j++) p[mod(a[i]-a[j])]++;
+	for(m=1;m<=M;m++) {
+		for(i=2;m*i<=M;i++) p[m] += p[m*i];
+		if(p[m] > k*(k+1)/2) continue;
+		t=0;
 		for(i=0;i<n;i++) {
-			v = a[i]%m;
-			if(!vis[v]) vis[v]=1;
-			else {
-				penal++;
-				if(penal>k) break;
-			}
+			if(vis[(a[i])%m]==m) t++; 
+			vis[(a[i])%m]=m;
 		}
-		if(penal<=k) break;
+		if(t<=k) break;
 	}
-	cout << m << endl;
+	cout<<m<<endl;
 	return 0;
 }
