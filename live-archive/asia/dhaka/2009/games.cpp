4a03125d19bb8adc0827f9215abc18d6e20dd321
jccarvalhosa <jocarvalhosa@gmail.com>
2013-07-26 12:23:16 -0300

    games

--- live-archive/asia/dhaka/2009/games.cpp
+++ live-archive/asia/dhaka/2009/games.cpp
@@ -5,80 +5,78 @@
 #include <cstring>
 #include <map>
 using namespace std;
+typedef vector<string> vs;
 
-string play(string s, int p) {
-	int q[32];
-	int vis[32];
+int dx[] = {1, 0, 0, -1};
+int dy[] = {0, 1, -1, 0};
+
+vs play(vs v, int x, int y) {
+	int q[16][2];
+	int vis[4][4];
 	memset(vis, 0, sizeof(vis));
 	int i=0, f=1;
-	q[0]=p;
-	vis[p]=1;
+	q[0][0] = x;
+	q[0][1] = y;
+	vis[x][y]=1;
 	while(i!=f) {
-		p=q[i++];
-		if(p%4!=0 && !vis[p-1] && s[p-1]==s[p]) {
-			vis[p-1] = 1;
-			q[f++] = p-1;
+		x=q[i][0];
+		y=q[i][1];
+		i++;
+		for(int k=0;k<4;k++) {
+			int nx = x+dx[k];
+			int ny = y+dy[k];
+			if(nx>=0 && nx<4 && ny>=0 && ny<4 && !vis[nx][ny] && v[nx][ny]==v[x][y]) {
+				q[f][0]=nx;
+				q[f][1]=ny;
+				vis[nx][ny]=1;
+				f++;
+			}
 		}
-		if(p%4!=3 && !vis[p+1] && s[p+1]==s[p]) {
-			vis[p+1] = 1;
-			q[f++] = p+1;
-		}
-		if(p>=4 && !vis[p-4] && s[p-4]==s[p]) {
-			vis[p-4] = 1;
-			q[f++] = p-4;
-		}
-		if(p<=11 && !vis[p+4] && s[p+4]==s[p]) {
-			vis[p+4] = 1;
-			q[f++] = p+4;
-		}
+		v[x][y]=' ';
 	}
-	for(i=0;i<f;i++) s[q[i]]=' ';
-	for(int i=0;i<4;i++) {
-		for(int j=3;j>=0;j--) if(s[i+4*j]==' ') {
+	for(int j=0;j<4;j++) {
+		for(int i=3;i>=0;i--) if(v[i][j]==' ') {
 			int x;
-			for(x=j-1;x>=0;x--) if(s[i+4*x]!=' ') break;
-			if(x>=0) swap(s[i+4*j], s[i+4*x]);
+			for(x=i-1;x>=0;x--) if(v[x][j]!=' ') break;
+			if(x>=0) swap(v[i][j], v[x][j]);
 		}
 	}
-	return s;
+	return v;
 }
 
-map<string, int> vis;
+map<vs, int> vis;
 
-int go(string s) {
-	int i;
-	for(i=0;i<16;i++) if(s[i]=='R' || s[i]=='B') break;
-	if(i==16) return 0;
-	if(vis.count(s)) return vis[s];
+int go(vs v) {
+	if(vis.count(v)) return vis[v];
 	int x=0;
-	for(i=0;i<16;i++) if(s[i]!=' ') {
-		string u = play(s, i);
+	for(int i=0;i<4;i++) for(int j=0;j<4;j++) if(v[i][j]!=' ') {
+		vs u = play(v, i, j);
 		int y = go(u);
+		y = -y;
 		if(y==0) x=1;
 		else if(y>0) {
-			if(x>0) continue;
-			y = -y;
-			x=min(x, y-1);
-		}
-		else {
-			y = -y;
 			if(x>0) x = min(x, y+1);
-			else x = y+1;
+			else x=y+1;
 		}
+		else if(x<=0) x = min(x, y-1);
 	}
-	return vis[s]=x;
+	return vis[v]=x;
 }
 
 int main() {
 	int T;
 	cin>>T;
 	for(int t=1;t<=T;t++) {
-		string s="", aux;
-		for(int i=0;i<4;i++) cin>>aux, s+=aux;
-		int v=go(s);
+		string s;
+		vs v;
+		for(int i=0;i<4;i++) {
+			cin>>s;
+			v.push_back(s);
+		}
+		int x=go(v);
 		cout<<"Case "<<t<<": ";
-		if(v>0) cout<<"win "<<v<<endl;
-		if(v<0) cout<<"loss "<<-v<<endl;
+		if(x>0) cout<<"win "<<x<<endl;
+		if(x<0) cout<<"loss "<<-x<<endl;
 	}
 	return 0;
 }
