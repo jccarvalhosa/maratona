c53185ae4d117dcb38974de52445f6fd03083624
jccarvalhosa <jocarvalhosa@gmail.com>
2013-07-26 21:59:23 -0300

    traffic jam

--- live-archive/asia/dhaka/2009/traffic.cpp
+++ live-archive/asia/dhaka/2009/traffic.cpp
@@ -6,13 +6,14 @@
 using namespace std;
 #define EPS 1e-6
 #define INF 1e9
+#define mp make_pair
 typedef pair<double, int> pdi;
 
 double cmpD(double a, double b=0) { return a<b+EPS? a<b-EPS ? -1: 0 : 1; }
 
 struct pt {
-	double x, y;
-	pt(double x=0, double y=0) : x(x), y(y) {}
+    double x, y;
+    pt(double x=0, double y=0) : x(x), y(y) {}
     pt operator +(pt p) { return pt(x+p.x, y+p.y); }
     pt operator -(pt p) { return pt(x-p.x, y-p.y); }
     pt operator *(double c) { return pt(x*c, y*c); }
@@ -32,12 +33,22 @@ struct pt {
 
     friend ostream& operator <<(ostream& o, pt p) { return o << "(" << p.x << "," << p.y << ")"; }
     
-	int prod(pt p, pt q) { return cmpD((*this-p) % (q-p)); }
+    int prod(pt p, pt q) { return cmpD((*this-p) % (q-p)); }
     bool between(pt p, pt q) { return prod(p, q) == 0 && min(p, q) <= *this && *this <= max(p, q); }
 };
 
+typedef pair<pt, pt> seg;
+
 pt project(pt a, pt b) { return b*((a*b)/(b*b)); }
 double abs(pt p, pt q = pt()) { return hypot(p.x-q.x, p.y-q.y); }
+
+seg angle_intersect(pt p, double teta, pt a, pt b) {
+	pt pp = a + project(p-a, b-a);
+	double d = abs(p, pp);
+	double x = d / tan(teta);
+	pt u = (b-a)/abs(b-a);
+	return mp(pp-u*x, pp+u*x);
+}
 
 pt p[1111];
 int m, vis[1111];
@@ -76,30 +87,20 @@ int main() {
 		for(int i=0;i<n;i++) cin>>p[i].x>>p[i].y;
 		m=n;
 		for(int i=0;i<n-1;i++) for(int j=0;j<n;j++) {
-			pt a = p[i];
-			pt b = p[i+1];
-			if(a>b) swap(a, b);
-			pt pp = a + project(p[j]-a, b-a);
-			double d = abs(p[j], pp);
-			pt u = (b-a)/abs(b-a);
-			double x = d / tan(acos(-1.0/f));
-			pt p1 = pp + u*x;
-			if(p1>a && p1<b) p[m++] = p1;
-			pt p2 = pp - u*x;
-			if(p2>a && p2<b) p[m++] = p2;
+			seg s = angle_intersect(p[j], acos(-1.0/f), p[i], p[i+1]);
+			if(s.first.between(p[i], p[i+1])) p[m++] = s.first;
+			if(s.second.between(p[i], p[i+1])) p[m++] = s.second;
 		}
 		for(int i=0;i<m;i++) for(int j=0;j<m;j++) {
 			adj[i][j] = abs(p[i], p[j]);
 			int flag=0;
-			for(int k=0;k<n-1;k++) 
-				if(p[i].between(p[k], p[k+1]) && p[j].between(p[k], p[k+1])) 
-					if(abs(p[k], p[i]) < abs(p[k], p[j])) 
-						flag=1;
+			for(int k=0;k<n-1;k++) if(p[i].between(p[k], p[k+1]) && p[j].between(p[k], p[k+1])) {
+				if(abs(p[k], p[i]) < abs(p[k], p[j])) flag=1;
+			}
 			if(!flag) adj[i][j] *= f;
 		}
 		dijkstra();
-		cout<<"Case "<<T++<<": ";
-		printf("%.9lf\n", d[n-1]);
+		printf("Case %d: %.9lf\n", T++, d[n-1]);
 	}
 	return 0;
 }
