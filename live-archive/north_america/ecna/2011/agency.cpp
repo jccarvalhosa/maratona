d51e661b0784656b0f4870ad8344c38c7b2c74d8
jccarvalhosa <jocarvalhosa@gmail.com>
2013-09-21 23:42:22 -0300

    agency

--- live-archive/north_america/ecna/2011/agency.cpp
+++ live-archive/north_america/ecna/2011/agency.cpp
@@ -2,17 +2,12 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
-#include <queue>
-#include <vector>
 using namespace std;
 typedef long long ll;
 typedef pair<int, int> pii;
 
-int mod(int a) { return a>0 ? a : -a; }
-
-int c[1111], vb[1111];
-ll sb[1111];
-pii va[1111];
+int c[1111];
+pii va[1111], vc[1111];
 
 int main() {
 	int n, T=1;
@@ -26,13 +21,10 @@ int main() {
 		int na=0;
 		for(int i=0;i<n;i++) if(a[i]=='1') va[na++] = pii(c[i], i);
 		sort(va, va+na);
-		int nb=0;
-		for(int i=0;i<n;i++) if(b[i]=='1') vb[nb++] = c[i];
-		sort(vb, vb+nb);
-		sb[0] = vb[0];
-		for(int i=1;i<nb;i++) sb[i] = sb[i-1] + vb[i];
+		for(int i=0;i<n;i++) vc[i] = pii(c[i], i);
+		sort(vc, vc+n);
 		ll best = 1ll<<62;
-		for(int t=0;t<=nb;t++) {
+		for(int t=0;t<=n;t++) {
 			a=aux;
 			int k=0;
 			ll sum=0;
@@ -40,11 +32,9 @@ int main() {
 			for(int i=0;i<n;i++) if(a[i]=='1') sum += c[i];
 			int diff=0;
 			for(int i=0;i<n;i++) if(a[i] != b[i]) diff++;
-			int pos=nb-1;
 			int j;
 			for(int j=na-1;j>=0;j--) {
 				int v = va[j].first, i=va[j].second;
-				while(pos != -1 && vb[pos] >= v) pos--;
 				if(b[i] == '0') {
 					ans += sum-v;
 					sum -= v;
@@ -59,11 +49,15 @@ int main() {
 					k++;
 				}
 			}
-			vector<int> v;
-			for(int i=0;i<n;i++) if(b[i]=='1' && a[i]=='0') v.push_back(c[i]);
-			sort(v.begin(), v.end());
-			for(int i=0;i<v.size();i++) ans += sum+v[i], sum+=v[i];
+			for(int j=0;j<n;j++) {
+				int v=vc[j].first, p=vc[j].second;
+				if(b[p]=='1' && a[p]=='0') {
+					sum += v;
+					ans += sum;
+				}
+			}
 			best = min(best, ans);
+			if(k != t) break;
 		}
 		printf("Case %d: ", T++);
 		cout<<best<<endl;
