249775d940bea52dd1b408a62e0ce8cf136c3ceb
jccarvalhosa <jocarvalhosa@gmail.com>
2013-09-25 21:50:25 -0300

    intervals

--- codeforces/gym/2013/episode3/intervals.cpp
+++ codeforces/gym/2013/episode3/intervals.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-#include <set>
 using namespace std;
 typedef pair<int, int> pii;
 #define M 50000
 
-int n, t[55555], a[55555], b[55555], c[55555];
+int n, val[55555], t[55555], a[55555], b[55555], c[55555];
 vector<pii> v;
-set<int> s;
 
-void update(int v) { 
-	for(;v<=M;v+=v&-v) t[v]++; 
+int update(int v) {
+	if(val[v]==1) return 0;
+	val[v]=1;
+	for(;v<=M;v+=v&-v) t[v]++;
+	return 1;
 }
+
 int query(int v) {
 	int ret=0;
 	for(;v;v-=v&-v) ret += t[v];
@@ -21,7 +23,6 @@ int query(int v) {
 
 int main() {
 	cin>>n;
-	for(int i=1;i<=M;i++) s.insert(i);
 	for(int i=0;i<n;i++) {
 		cin>>a[i]>>b[i]>>c[i];
 		v.push_back(pii(b[i], i));
@@ -29,15 +30,8 @@ int main() {
 	sort(v.begin(), v.end());
 	for(int i=0;i<v.size();i++) {
 		int p = v[i].second;
-		int used = query(b[p]) - query(a[p]-1);
-		c[p] -= used;
-		if(c[p] <= 0) continue;
-		set<int>::iterator it=s.upper_bound(b[p]); it--;
-		for(int j=0;j<c[p];j++) {
-			update(*it);
-			if(it == s.begin()) s.erase(it);
-			else s.erase(it--);
-		}
+		c[p] -= query(b[p]) - query(a[p]-1);
+		for(int j=0;c[p]>0;j++) if(update(b[p]-j)) c[p]--;
 	}
 	cout<<query(M)<<endl;
 	return 0;
