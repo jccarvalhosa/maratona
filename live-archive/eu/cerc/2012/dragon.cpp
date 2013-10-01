cfc298cacf9aaa1d2d60339774c9c95827a29264
jccarvalhosa <jocarvalhosa@gmail.com>
2013-09-30 23:27:47 -0300

    dragon and knights

--- live-archive/eu/cerc/2012/dragon.cpp
+++ live-archive/eu/cerc/2012/dragon.cpp
@@ -1,12 +1,15 @@
 #include <cstdio>
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 typedef vector<int> vi;
 typedef long long ll;
 
-int x[55555], y[55555];
+int l, x[55555], y[55555];
 ll a[111], b[111], c[111];
+
+bool cmp(int k) { return a[l]*x[k] + b[l]*y[k] + c[l] > 0; }
 
 int main() {
 	int T;
@@ -18,26 +21,16 @@ int main() {
 		int p=0;
 		for(int i=0;i<n;i++) for(int j=i+1;j<n;j++) if(a[i]*b[j] != a[j]*b[i]) p++;
 		for(int i=0;i<m;i++) cin>>x[i]>>y[i];
-		vi v;
-		vector<vi> space;
-		for(int i=0;i<m;i++) v.push_back(i);
-		space.push_back(v);
-		int i;
-		for(i=0;i<n;i++) {
-			vector<vi> newspace;
-			for(int j=0;j<space.size();j++) {
-				vi u1, u2, v = space[j];
-				for(int jj=0;jj<v.size();jj++) {
-					int k = v[jj];
-					if(a[i]*x[k] + b[i]*y[k] + c[i] > 0) u1.push_back(k);
-					else u2.push_back(k);
-				}
-				if(!u1.empty()) newspace.push_back(u1);
-				if(!u2.empty()) newspace.push_back(u2);
-			}
-			space = newspace;
+		vi v(m);
+		for(int i=0;i<m;i++) v[i]=i;
+		vector<vi> reg(1, v);
+		for(l=0;l<n;l++) for(int j=reg.size()-1;j>=0;j--) {
+			int d = partition(reg[j].begin(), reg[j].end(), cmp) - reg[j].begin();
+			if(d == 0 || d == reg[j].size()) continue;
+			reg.push_back(vi(reg[j].begin()+d, reg[j].end()));
+			reg[j].resize(d);
 		}
-		if(space.size() == p+n+1) cout<<"PROTECTED"<<endl;
+		if(reg.size() == p+n+1) cout<<"PROTECTED"<<endl;
 		else cout<<"VULNERABLE"<<endl;
 	}
 	return 0;
