44654f2a7fbd71e526b9c4a66f2fdae44e2eaff7
jccarvalhosa <jocarvalhosa@gmail.com>
2014-05-06 08:24:30 -0300

    interfering segments

--- timus/interfering.cpp
+++ timus/interfering.cpp
@@ -29,31 +29,6 @@ struct pt {
 	bool left(pt p, pt q) { return prod(p, q) < 0; }
 }; 
 
-struct ptlong {
-	ll x, y;
-	ptlong(ll x=0, ll y=0) :x(x), y(y){}
-	ll operator % (ptlong p) { return x*p.y - y*p.x; }
-	ptlong operator - (ptlong p) { return ptlong(x-p.x, y-p.y); }
-	ptlong operator + (ptlong p) { return ptlong(x+p.x, y+p.y); }
-	ptlong operator * (ll c) { return ptlong(x*c, y*c); }
-	ptlong operator / (ll c) { return ptlong(x/c, y/c); }
-	int cmp(ptlong p) const {
-		if(x < p.x) return -1;
-		if(x > p.x) return 1;
-		if(y < p.y) return -1;
-		if(y > p.y) return 1;
-		return 0;
-	}
-	bool operator < (const ptlong &p) const { return cmp(p) < 0; }
-	bool operator <= (const ptlong &p) const { return cmp(p) <= 0; }
-	bool operator > (const ptlong &p) const { return cmp(p) > 0; }
-	bool operator >= (const ptlong &p) const { return cmp(p) >= 0; }
-	bool operator == (const ptlong &p) const { return cmp(p) == 0; }
-	ll prod(ptlong p, ptlong q) { return (*this-p) % (q-p); }
-	bool between(ptlong p, ptlong q) { return prod(p, q) == 0 && min(p, q) <= *this && *this <= max(p, q); }
-	bool left(ptlong p, ptlong q) { return prod(p, q) < 0; }
-}; 
-
 int mod(int a) { return a > 0 ? a : -a; }
 int area(pt a, pt b, pt c) { return mod(b%c + a%b - a%c); }
 
@@ -67,7 +42,6 @@ inline bool in(pt p, pt a, pt b, pt c) {
 }
 
 pt p[1111];
-ptlong plong[1111];
 
 int direction(pt pi, pt pj, pt pk)
 {
@@ -98,13 +72,13 @@ int segments_intersect(pt p1, pt p2, pt p3, pt p4)
 	
 int n;
 
-bool cart(ptlong a, ptlong b, ptlong c, ptlong d) { return d.left(a, c) != d.left(b, c) && c.left(a, b) != d.left(a, b); }
+bool cart(pt a, pt b, pt c, pt d) { return d.left(a, c) != d.left(b, c) && c.left(a, b) != d.left(a, b); }
 
-bool in_poly(ptlong a) {
-	for(int i=1;i<=n;i++) if(a.between(plong[i], plong[i+1 > n ? 1 : i+1])) return -1;
+bool in_poly(pt a) {
+	for(int i=1;i<=n;i++) if(a.between(p[i], p[i+1 > n ? 1 : i+1])) return -1;
 	int c=0;
-	ptlong ref(100002, 100002);
-	for(int i=1;i<=n;i++) c += cart(plong[i], plong[i+1 > n ? 1 : i+1], a, ref);;
+	pt ref(20002, 20002);
+	for(int i=1;i<=n;i++) c += cart(p[i], p[i+1 > n ? 1 : i+1], a, ref);;
 	return c&1;
 }
 
@@ -115,24 +89,21 @@ bool ok(int i, int j, int k) {
 		if(ii!=i && ii!=k && jj!=i && jj!=k && segments_intersect(p[ii], p[jj], p[i], p[k])) return 0;
 		if(ii!=k && ii!=j && jj!=k && jj!=j && segments_intersect(p[ii], p[jj], p[j], p[k])) return 0;
 	}
-	if(!in_poly((plong[i]+plong[j])/2)) return 0;
-	if(!in_poly((plong[i]+plong[k])/2)) return 0;
-	if(!in_poly((plong[j]+plong[k])/2)) return 0;
-	if(!in_poly((plong[i]+plong[j]+plong[k])/3)) return 0;
+	if(!in_poly((p[i]+p[j])/2)) return 0;
+	if(!in_poly((p[i]+p[k])/2)) return 0;
+	if(!in_poly((p[j]+p[k])/2)) return 0;
 	return 1;
 }
 
 void get(pt &p) {
 	scanf("%d%d", &p.x, &p.y);
+	p.x *= 2;
+	p.y *= 2;
 }
 
 int main() {
 	cin>>n;
 	for(int i=1;i<=n;i++) get(p[i]);
-	for(int i=1;i<=n;i++) {
-		plong[i].x = p[i].x*6;
-		plong[i].y = p[i].y*6;
-	}
 	pt a, b;
 	get(a);
 	get(b);
