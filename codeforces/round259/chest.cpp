23ba5657044a591bd451251627b1a9b4628e11d9
Jonathan Carvalhosa <jocarvalhosa@gmail.com>
2014-09-06 20:18:26 -0300

    chest

--- codeforces/round259/chest.cpp
+++ codeforces/round259/chest.cpp
@@ -8,12 +8,14 @@ int a[111], nprimes, vis[60], mask[60], dp[101][200000];
 int mod(int x) { return x > 0 ? x : -x; }
 
 int main() {
+	int all=0;
 	for(int i=2;i<60;i++) {
 		if(!vis[i]) {
 			for(int j=i;j<60;j+=i) {
 				vis[j]=1;
 				mask[j] += 1<<nprimes;
 			}
+			all += 1<<nprimes;
 			nprimes++;
 		}
 	}
@@ -25,10 +27,13 @@ int main() {
 	for(int i=0;i<n;i++) for(int j=0;j<1<<nprimes;j++) dp[i][j]=1e9;
 	for(int j=0;j<1<<nprimes;j++) dp[n][j]=0;
 	for(int i=n-1;i>=0;i--) 
-		for(int v=1;v<2*a[i];v++) 
-			for(int avail=0;avail<1<<nprimes;avail++) 
-				if((mask[v] & avail) == mask[v]) 
-					dp[i][avail] = min(dp[i][avail], mod(v - a[i]) + dp[i+1][avail - mask[v]]);
+		for(int v=1;v<2*a[i];v++) { 
+			int x = all - mask[v];
+			for(int extra = x; ; extra = (extra-1) & x) {
+				dp[i][extra + mask[v]] = min(dp[i][extra + mask[v]], mod(v - a[i]) + dp[i+1][extra]);
+				if(!extra) break;
+			}
+		}
 
 	int avail = (1<<nprimes)-1;
 	
